beaf3d64843a170f18633a1598ea0f8b7d96271d
VenBak <andre.brahin@gmail.com>
2024-10-23 12:08:12 +0200

    Added some logging for debugging

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@
 using namespace std;
 
 // size of the image
-const int64_t SIZE = 16384ULL;
+const int64_t SIZE = 10ULL;
 const int64_t REPEAT = 1ULL;
 
 int main(int argc, char* argv[]) {
@@ -27,6 +27,7 @@ int main(int argc, char* argv[]) {
   for (int64_t i = 0; i < SIZE; i++) {
     for (int64_t j = 0; j < SIZE; j++) {
       img[j * SIZE + i] = (2 * j + i) % 32768;
+      cout << "Array contents:" << img[j * SIZE + i] << " at index:" << j* SIZE + i << endl;
     }
   }
 
@@ -40,27 +41,86 @@ int main(int argc, char* argv[]) {
   // Apply an averaging imaging filter to some input image, and write in to an output image.
   // A pixel in the output image is calculated by averaging 9 pixels: the pixel at the same
   // coordinates in the input image, and the adjecent pixels.
-  for (int64_t r = 0; r < REPEAT; ++r) {
-    for (int64_t i = 1; i < SIZE - 1; i++) {
-      for (int64_t j = 1; j < SIZE - 1; j++) {
+  for (int64_t r = 0; r < 1; ++r) {
+    for (int64_t i = 1; i < 5 - 1; i++) {
+      for (int64_t j = 1; j < 5 - 1; j++) {
         // Set all values of the res array to 0 initialized
-        res[j * SIZE + i] = 0;
-        if (j==1) {
+        // res[j * SIZE + i] = 0;
+        if (i*j==1) {
           for (long k = -1; k < 2; k++) {
             for (long l = -1; l < 2; l++) {
               res[j * SIZE + i] += img[(j + l) * SIZE + i + k];
+              cout << "Added: " << img[(j + l) * SIZE + i + k] << " to " << res[j * SIZE + i] << " at " << j* SIZE+i << endl;
             }
           }
-          res[j * SIZE + i] /= 9;
-        } else {
-          cache = res[j-1 * SIZE + i] * 9;
+          float nuber =res[j * SIZE + i] /= 9;
+          cout << "First averaging value: " << nuber << endl;
+          cout << "Index of "<< j*i <<  " averaging: " << j * SIZE + i << endl;
+        } else if (j * SIZE + i - SIZE > 0){
+          cache = res[j * SIZE + i - SIZE] * 9;
+          cout << "New index: " << j * SIZE + i << endl;
+          cout << "Previous average: " << cache  << " index: " << j*SIZE +i - SIZE << endl;
+          cout << "New: " << res[j * SIZE + i] << endl;
           for (long k = -1; k < 2; k++) {
+
             cache -= img[(j-2 ) * SIZE + i + k];
-            cache += img[(j+1) * SIZE + i + k];
+            cout << "removed: " << img[(j-2 ) * SIZE + i + k] << " at index: " << (j-2 ) * SIZE + i + k << endl;
+
+            if ((j+1) * SIZE + i + k != SIZE * SIZE - 1) {
+              cache += img[(j+1) * SIZE + i + k];
+              cout << "added: " << img[(j-2 ) * SIZE + i + k] << " at index " << (j+1) * SIZE + i + k << endl;
+            }
           }
-          res[j * SIZE + i] = cache/9;
+          res[j * SIZE + i] = cache / 9;
         }
-        cache = res[j * SIZE + i];
+      }
+    }
+
+    cout << "\n Default functionality: \n" << endl;
+    for (int64_t i = 1; i < 5 - 1; i++) {
+      for (int64_t j = 1; j < 5 - 1; j++) {
+        // Set all values of the res array to 0 initialized
+        res[j * SIZE + i] = 0;
+        
+        for (long k = -1; k < 2; k++) {
+          for (long l = -1; l < 2; l++) {
+            res[j * SIZE + i] += img[(j + l) * SIZE + i + k];
+            cout << " (img) added: " << img[(j + l) * SIZE + i + k] << " at index "<< (j + l) * SIZE + i + k;
+            cout << res[j * SIZE + i] << " at index: " << j * SIZE + i << endl;
+          }
+        }
+        res[j * SIZE + i] /= 9;
+        cout << "------------------------------" << endl;
+      }
+    }
+
+    cout << "\n Semi-Default functionality: \n" << endl;
+    for (int64_t i = 1; i < 5 - 1; i++) {
+      for (int64_t j = 1; j < 5 - 1; j++) {
+        // Set all values of the res array to 0 initialized
+        res[j * SIZE + i] = 0;
+        // First average
+        if (i* j == 1) {
+          for (long k = -1; k < 2; k++) {
+            for (long l = -1; l < 2; l++) {
+              res[j * SIZE + i] += img[(j + l) * SIZE + i + k];
+              cout << " (img) added: " << img[(j + l) * SIZE + i + k] << " at index "<< (j + l) * SIZE + i + k;
+              cout << res[j * SIZE + i] << " at index: " << j * SIZE + i << endl;
+            }
+          }
+        } else if (j * SIZE + i - SIZE > 0) {
+          cout << j * SIZE + i - SIZE << endl;
+          for (long k = -1; k < 2; k++) {
+            for (long l = -1; l < 2; l++) {
+              res[j * SIZE + i] += img[(j + l) * SIZE + i + k];
+              cout << " (img) added: " << img[(j + l) * SIZE + i + k] << " at index "<< (j + l) * SIZE + i + k;
+              cout << res[j * SIZE + i] << " at index: " << j * SIZE + i << endl;
+            }
+          }
+        }
+      
+        res[j * SIZE + i] /= 9;
+        cout << "------------------------------" << endl;
       }
     }
 
